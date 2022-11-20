0847e011b32c857c8efa34f6d8be12a3703fa4e8
Rita Zaborowska <rm.zaborowska@gmail.com>
2022-11-20 18:15:46 +0100

    Adding result function

--- rps_app.js
+++ rps_app.js
@@ -1,31 +1,81 @@
 const showPlayerChoice = document.getElementById("playerChoice")
 const showComputerChoice = document.getElementById("computerChoice")
-const showresult = document.getElementById("result")
+const showResult = document.getElementById("result")
 const choices = document.getElementsByClassName("button")
 let playerChoice;
 let computerChoice;
+let result;
 
-
-console.log("dupa")
-console.log(showComputerChoice)
-console.log(showPlayerChoice)
-console.log(showresult)
-console.log(choices)
-console.log(playerChoice)
-
-function generateComputerChoice() {
-    const generatedNumber = Math.floor(Math.random() * 3)
-    switch (generatedNumber) {
-        case 1:
-
+function giveResult() {
+    if (playerChoice === computerChoice) {
+        result = "It'a draw"
+    }
+   
+    if (playerChoice === "rock") {
+        switch (computerChoice) {
+            case "paper":
+                result = "You lose!"
+                break
+            case "scissors":
+                result = "You win!"
+                break
+        }
+    }
+    if (playerChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                result == "You win!"
+                break
+            case "scissors":
+                result == "You lose!"
+                break
+        }
+    }
+    if (playerChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                result == "You lose!"
+                break
+            case "paper":
+                result == "You win!"
+                break
+        }
+    }
+                
+     
+        
+    console.log(result)
+    showResult.innerHTML = result
+}
+        
+    
+    function generateComputerChoice() {
+        const generatedNumber = Math.floor(Math.random() * 3)
+        switch (generatedNumber) {
+            case 0:
+                computerChoice = "rock"
+                break
+            case 1:
+                computerChoice = "paper"
+                break
+            case 2:
+                computerChoice = "scissors"
+                break
+        }
+        
+        showComputerChoice.innerHTML = computerChoice;
     }
 
-    //this three is because we have three choices(paper, rock and scissors), we can also write here choices.length
-}
-Array.from(choices).forEach(choice => choice.addEventListener("click", (e) => {
-    playerChoice = e.target.id
-    showPlayerChoice.innerHTML = playerChoice
-    generateComputerChoice()
     
-}
-))
+
+    //this three is because we have three choices(paper, rock and scissors), we can also write here choices.length
+
+Array.from(choices).forEach(choice => choice.addEventListener("click", (e) => {
+        playerChoice = e.target.id
+        showPlayerChoice.innerHTML = playerChoice
+        generateComputerChoice()
+        giveResult()
+    }))
+    
+
+
