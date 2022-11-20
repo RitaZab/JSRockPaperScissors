2bfc4701aba02c1620534812601399b9816170ce
Rita Zaborowska <rm.zaborowska@gmail.com>
2022-11-20 17:41:21 +0100

    Adding first JS functions

--- rps_app.js
+++ rps_app.js
@@ -1,1 +1,31 @@
-let playerChoice
+const showPlayerChoice = document.getElementById("playerChoice")
+const showComputerChoice = document.getElementById("computerChoice")
+const showresult = document.getElementById("result")
+const choices = document.getElementsByClassName("button")
+let playerChoice;
+let computerChoice;
+
+
+console.log("dupa")
+console.log(showComputerChoice)
+console.log(showPlayerChoice)
+console.log(showresult)
+console.log(choices)
+console.log(playerChoice)
+
+function generateComputerChoice() {
+    const generatedNumber = Math.floor(Math.random() * 3)
+    switch (generatedNumber) {
+        case 1:
+
+    }
+
+    //this three is because we have three choices(paper, rock and scissors), we can also write here choices.length
+}
+Array.from(choices).forEach(choice => choice.addEventListener("click", (e) => {
+    playerChoice = e.target.id
+    showPlayerChoice.innerHTML = playerChoice
+    generateComputerChoice()
+    
+}
+))
