b70740a817a3d8765d87aa3c4c8c62cc66138076
Rita Zaborowska <rm.zaborowska@gmail.com>
2022-11-22 13:56:43 +0100

    Adding showing icon functions

--- rps_app.js
+++ rps_app.js
@@ -4,17 +4,17 @@ const showResult = document.getElementById("result")
 const choices = document.getElementsByClassName("button")
 const pPoints = document.getElementById("player-points")
 const cPoints = document.getElementById("computer-points")
-const rockicon = document.createElement("img")
-rockicon.src="icons/stone.png"
-const papericon = document.createElement("img")
-papericon.src="icons/paper.png"
-const scissorsicon = document.createElement("img")
-scissorsicon.src="icons/scissors.png"
 let playerChoice;
 let computerChoice;
 let result;
 let ppoints = 0;
-let cpoints = 0
+let cpoints = 0;
+let activeicons = document.getElementsByClassName("active")
+console.log(activeicons)
+
+
+    
+
 
 function giveResult() {
     if (playerChoice === computerChoice) {
@@ -59,10 +59,31 @@ function giveResult() {
 }
         
 function showingPlayerChoice() {
+    Array.from(activeicons).forEach((el)=>el.classList.remove("active"))
     if (playerChoice === "rock") {
-        document.querySelector(".player-icon").appendChild(rockicon)
+        
+        document.querySelector("#prock").classList.add("active")
     }
-}   
+    if (playerChoice === "paper") {
+            document.querySelector("#ppaper").classList.add("active")
+        }
+    if (playerChoice === "scissors") {
+        document.querySelector("#pscissors").classList.add("active")
+    }
+}
+function showingComputerChoice() {
+    if (computerChoice === "rock") {
+        
+        document.querySelector("#crock").classList.add("active")
+    }
+    if (computerChoice === "paper") {
+            document.querySelector("#cpaper").classList.add("active")
+        }
+    if (computerChoice === "scissors") {
+        document.querySelector("#cscissors").classList.add("active")
+    }
+    
+}
 function generateComputerChoice() {
     const generatedNumber = Math.floor(Math.random() * 3)
     
@@ -70,19 +91,19 @@ function generateComputerChoice() {
             case 0:
                 computerChoice = "rock"
                 
-                document.querySelector(".computer-icon").appendChild(rockicon)
+                
                 break
             case 1:
                 computerChoice = "paper"
-                document.querySelector(".computer-icon").appendChild(papericon)
+                
                 break
             case 2:
                 computerChoice = "scissors"
-                document.querySelector(".computer-icon").appendChild(scissorsicon)
+                
                              
                 break
         }
-        
+        console.log(computerChoice)
         showComputerChoice.innerHTML = computerChoice;
     }
 function countingPoints(){
@@ -97,7 +118,8 @@ function countingPoints(){
         }
     }
 }
-    
+
+
 
     //this three is because we have three choices(paper, rock and scissors), we can also write here choices.length
 
@@ -105,8 +127,10 @@ Array.from(choices).forEach(choice => choice.addEventListener("click", (e) => {
     playerChoice = e.target.id
     
     showPlayerChoice.innerHTML = playerChoice
+    
     showingPlayerChoice()
     generateComputerChoice()
+    showingComputerChoice()
     giveResult()
     countingPoints()
     }))
